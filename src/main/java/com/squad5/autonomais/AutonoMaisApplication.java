fbf646dcacf8909b9c859b0af978144c730fe413
Fernando Sabalete <fb.sabalete@gmail.com>
2021-02-19 10:28:54 -0300

    Criação do projeto Spring

---
+++ src/main/java/com/squad5/autonomais/AutonoMaisApplication.java
@@ -0,0 +1,13 @@
+package com.squad5.autonomais;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutonoMaisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutonoMaisApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/squad5/autonomais/AutonoMaisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.squad5.autonomais;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutonoMaisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
