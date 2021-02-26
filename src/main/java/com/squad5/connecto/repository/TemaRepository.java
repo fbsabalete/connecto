0c6890cd7b0368e79766473d5b4c324d1a4d7641
Victor Araujo <Victor>
2021-02-26 10:28:29 -0300

    (feat):Criado TemaRepository #2

---
+++ src/main/java/com/squad5/connecto/repository/TemaRepository.java
@@ -0,0 +1,9 @@
+package com.squad5.connecto.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.squad5.connecto.model.Tema;
+
+public interface TemaRepository extends JpaRepository<Tema, Long> {
+
+}
