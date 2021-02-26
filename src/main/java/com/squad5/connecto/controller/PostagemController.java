4103468f1607ebde38313e39d367bd017a4065f2
Santos307 <candidocavaleiro@outlook.com>
2021-02-26 15:30:09 -0300

    feat(Controller): Criado Método put #3

--- src/main/java/com/squad5/connecto/controller/PostagemController.java
+++ src/main/java/com/squad5/connecto/controller/PostagemController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -44,6 +45,10 @@ public class PostagemController {
 	@PostMapping
 	public ResponseEntity<Postagem> post(@RequestBody Postagem postagem){
 	  return ResponseEntity.status(HttpStatus.CREATED).body(repository.save(postagem)); 
+	}
+	@PutMapping
+	public ResponseEntity<Postagem> put(@RequestBody Postagem postagem){
+		return ResponseEntity.status(HttpStatus.OK).body(repository.save(postagem));
 	}
 
 }
