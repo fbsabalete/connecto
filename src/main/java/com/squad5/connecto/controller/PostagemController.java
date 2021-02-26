7b6ac0ec6d6a7501c698b32b3252e16d2f5933e6
Santos307 <candidocavaleiro@outlook.com>
2021-02-26 15:12:27 -0300

    feat(controller): Criado Método post #3

--- src/main/java/com/squad5/connecto/controller/PostagemController.java
+++ src/main/java/com/squad5/connecto/controller/PostagemController.java
@@ -3,11 +3,14 @@ package com.squad5.connecto.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.jmx.export.annotation.ManagedAttribute;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -34,10 +37,13 @@ public class PostagemController {
 	}
 
 	@GetMapping("/titulo/{titulo}")
-	public ResponseEntity<List<Postagem>> getByTitulo(@PathVariable String titulo){
+	public ResponseEntity<List<Postagem>> getByTitulo(@PathVariable String titulo) {
 		return ResponseEntity.ok(repository.findAllByTituloContainingIgnoreCase(titulo));
-		
-		
-		
 	}
+
+	@PostMapping
+	public ResponseEntity<Postagem> post(@RequestBody Postagem postagem){
+	  return ResponseEntity.status(HttpStatus.CREATED).body(repository.save(postagem)); 
+	}
+
 }
