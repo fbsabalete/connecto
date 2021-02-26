f0ec24b72a9ccc9b9871805457e982ce95da8e75
Victor Araujo <Victor>
2021-02-26 12:11:33 -0300

    (feat): Criado Método Delete #2

--- src/main/java/com/squad5/connecto/controller/TemaController.java
+++ src/main/java/com/squad5/connecto/controller/TemaController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -49,6 +50,10 @@ public class TemaController {
 	public ResponseEntity<Tema> put(@RequestBody Tema tema) {
 		return ResponseEntity.ok(repository.save(tema));
 
+	}
+	@DeleteMapping("/{id}") 
+	public void delete(@PathVariable long id) {
+		repository.deleteById(id);
 	}
 
 }
