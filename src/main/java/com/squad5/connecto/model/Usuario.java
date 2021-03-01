cdf1c30ea1157d9d26fb6b1f02bda81671e0cb67
Gustavo Lima <gustavo1.l@hotmail.com>
2021-03-01 09:49:59 -0300

    feat(usuario): Criado model e repository de Usuario #5

---
+++ src/main/java/com/squad5/connecto/model/Usuario.java
@@ -0,0 +1,59 @@
+package com.squad5.connecto.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.Size;
+
+import com.sun.istack.NotNull;
+
+@Entity
+@Table(name = "tb_usuario")
+public class Usuario {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+	
+	@NotNull
+	@Size(min = 5, max = 40)
+	private String nomeCompleto;
+	
+	@NotNull
+	@Email
+	@Size(min = 5, max = 40)
+	private String email;
+	
+	@NotNull
+	@Size(min = 5, max = 40)
+	private String senha;
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public String getNomeCompleto() {
+		return nomeCompleto;
+	}
+	public void setNomeCompleto(String nomeCompleto) {
+		this.nomeCompleto = nomeCompleto;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getSenha() {
+		return senha;
+	}
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+	
+	
+}
---
+++ src/main/java/com/squad5/connecto/repository/UsuarioRepository.java
@@ -0,0 +1,9 @@
+package com.squad5.connecto.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.squad5.connecto.model.Usuario;
+
+public interface UsuarioRepository extends JpaRepository<Usuario, Long>{
+
+}
