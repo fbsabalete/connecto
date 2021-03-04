fe25c933a84254174b4f69a449e36d928a4994f6
Fernando Sabalete <fb.sabalete@gmail.com>
2021-03-04 11:26:41 -0300

    feat(UsuarioService): Criado o método logar #7

--- src/main/java/com/squad5/connecto/service/UsuarioService.java
+++ src/main/java/com/squad5/connecto/service/UsuarioService.java
@@ -2,9 +2,14 @@ package com.squad5.connecto.service;
 
 import com.squad5.connecto.model.Usuario;
 import com.squad5.connecto.repository.UsuarioRepository;
+import org.apache.commons.codec.binary.Base64;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+import java.util.Optional;
 
 @Service
 public class UsuarioService {
@@ -23,5 +28,24 @@ public class UsuarioService {
         usuario.setSenha(senhaEncoder);
 
         return repository.save(usuario);
+    }
+
+    public Optional<UserLogin> logar(Optional<UserLogin> user){
+        BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+        Optional<Usuario> usuario = repository.findByEmailIgnoreCase(user.get().getUsuario());
+
+        if(usuario.isPresent()){
+            if(encoder.matches(user.get().getSenha(), usuario.get().getSenha())){
+                String auth = user.get().getUsuario() + ":" + user.get().getSenha();
+                byte[] encodedAuth = Base64.encodeBase64(auth.getBytes(Charset.forName("US-ASCII")));
+                String authHeader = "Basic " + new String(encodedAuth);
+
+                user.get().setToken(authHeader);
+                user.get().setUsuario(usuario.get().getNomeCompleto());
+
+                return user;
+            }
+        }
+        return null;
     }
 }
