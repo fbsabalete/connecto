f08ea09792086324412df871101652cbe3217966
Fernando Sabalete <fb.sabalete@gmail.com>
2021-03-04 11:11:17 -0300

    refact(UsuarioRepository): Adicionado método de busca por email #7

--- src/main/java/com/squad5/connecto/repository/UsuarioRepository.java
+++ src/main/java/com/squad5/connecto/repository/UsuarioRepository.java
@@ -4,6 +4,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.squad5.connecto.model.Usuario;
 
-public interface UsuarioRepository extends JpaRepository<Usuario, Long>{
+import java.util.Optional;
 
+public interface UsuarioRepository extends JpaRepository<Usuario, Long>{
+    public Optional<Usuario> findByEmailIgnoreCase(String email);
 }
