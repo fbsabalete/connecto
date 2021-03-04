302160e604bdf232a2197c0eb8e7f15e73444357
Leo <leocintra26@gmail.com>
2021-03-04 11:26:25 -0300

    refact(UsuarioController): Importado classe UsuarioLogin #8

--- src/main/java/com/squad5/connecto/controller/UsuarioController.java
+++ src/main/java/com/squad5/connecto/controller/UsuarioController.java
@@ -1,6 +1,7 @@
 package com.squad5.connecto.controller;
 
 import com.squad5.connecto.model.Usuario;
+import com.squad5.connecto.model.UsuarioLogin;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
