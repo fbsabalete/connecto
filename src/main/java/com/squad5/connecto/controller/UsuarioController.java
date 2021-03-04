63be31beeca97a8d6ccc3920b009ace2c670369e
Leo <leocintra26@gmail.com>
2021-03-04 11:17:06 -0300

    feat(UsuarioController): Criado controller usuario #8

---
+++ src/main/java/com/squad5/connecto/controller/UsuarioController.java
@@ -0,0 +1,37 @@
+package com.squad5.connecto.controller;
+
+import com.squad5.connecto.model.Usuario;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/usuarios")
+@CrossOrigin("*")
+public class UsuarioController {
+
+    @Autowired
+    private UsuarioService usuarioService;
+
+    @PostMapping("/logar")
+    public ResponseEntity<UsuarioLogin> Autencation(@ResponseBody Optional<UsuarioLogin> user){
+        return usuarioService.Logar(user).map(resp -> ResponseEntity.ok(resp))
+                .orElse(ResponseEntity.status(HttpStatus.UNAUTHORIZED).build());
+    }
+
+    @PostMapping("/cadastrar")
+    public ResponseEntity<Usuario> Post(@RequestBody Usuario usuario){
+        Optional<Usuario> user = Optional.ofNullable(usuarioService.CadastrarUsuario(usuario));
+
+        try {
+            return ResponseEntity.ok(user.get());
+        } catch (Exception e) {
+            return ResponseEntity.badRequest().build();
+        }
+
+    }
+
+}
