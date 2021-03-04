d29e689614a9f9327af3e563f0406a1de03cc347
Leo <leocintra26@gmail.com>
2021-03-04 11:29:06 -0300

    feact(Models): Criando classe UsuarioLogin #6

---
+++ src/main/java/com/squad5/connecto/model/UsuarioLogin.java
@@ -0,0 +1,42 @@
+package com.squad5.connecto.model;
+
+
+public class UsuarioLogin {
+
+    private String nomeCompleto;
+    private String email;
+    private String senha;
+    private String token;
+
+    public String getNomeCompleto() {
+        return nomeCompleto;
+    }
+
+    public void setNomeCompleto(String nomeCompleto) {
+        this.nomeCompleto = nomeCompleto;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+}
