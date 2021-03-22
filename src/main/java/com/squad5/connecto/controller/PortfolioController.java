400b15f152c3f4df793d693e837a06360c7ba74b
Fernando Sabalete <fb.sabalete@gmail.com>
2021-03-22 12:35:29 -0300

    feat(portfolio): Criado o PortfolioController com CRUD completo #12

---
+++ src/main/java/com/squad5/connecto/controller/PortfolioController.java
@@ -0,0 +1,43 @@
+package com.squad5.connecto.controller;
+
+import com.squad5.connecto.model.Portfolio;
+import com.squad5.connecto.repository.PortfolioRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/portfolio")
+public class PortfolioController {
+
+    @Autowired
+    private PortfolioRepository repository;
+
+    @GetMapping
+    public ResponseEntity<List<Portfolio>> getAll() {
+        return ResponseEntity.ok(repository.findAll());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Portfolio> getById(@PathVariable long id) {
+        return repository.findById(id).map(resp -> ResponseEntity.ok(resp)).orElse(ResponseEntity.notFound().build());
+    }
+
+    @PostMapping
+    public ResponseEntity<Portfolio> post(@RequestBody Portfolio portfolio) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(repository.save(portfolio));
+    }
+
+    @PutMapping
+    public ResponseEntity<Portfolio> put(@RequestBody Portfolio portfolio) {
+        return ResponseEntity.status(HttpStatus.OK).body(repository.save(portfolio));
+    }
+
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable long id) {
+        repository.deleteById(id);
+    }
+}
